62b1c7351192ce5b84397be7e4cb1c3caf5a4820
David Acerbo <david.acerbo@gmail.com>
2019-11-28 13:09:10 -0300

    Primer grafico con almenos un valor traido desde la mongoDB

--- pywords.py
+++ pywords.py
@@ -25,6 +25,7 @@ def get_files_collection(path):
     except Exception as e:
         print(mark+"get_collection_in_format : {}".format(e))
 
+
 ## Por cada archivo/colecion orquesto el procesamiento
 def processing_files(filesNames, path):
     try:
@@ -64,8 +65,6 @@ def construc_document(wordsCount, fileName):
     except Exception as e:
         print(mark+"construc_document: {} -- {}".format(e,fileName))
 
-        
-
 
 ## Deberia subir a la BASE MONGO    
 def insert_to_db(document,fileName):
@@ -74,20 +73,25 @@ def insert_to_db(document,fileName):
     except Exception as e:
         print(mark+"insert_to_db: {} -- {}".format(e,fileName))
 
-## Querys a Mongo DB
+
+## Querys a Mongo DB###########################
 def query_mongoDB_count_document():
     return auth_mongo.count()
 
+
 def query_mongoDB_distinct_words():
-    return auth_mongo.count()
+    return 123
+
 
 def query_mongoDB_document_more_words():
-    return auth_mongo.count()
+    return "texto1.txt"
+
 
 def query_top_ten_collection():
-    return "OBJETO JSON"
+    return {('Hola','Casa','Perro','GO','La','queso','mono','zorro','GOy','Laa')}
+## ^^ Querys a Mongo DB###########################
 
-
+## consultar reloj
 def get_time():
     try:
         from time import time
@@ -96,9 +100,64 @@ def get_time():
     except Exception as e:
         print(mark+"get_time: {}".format(e))
 
-def pygraph_reports():
-    pass
-    
+## mostrar grafico recibe (qty_documents,document_more_words,qty_distinct_words,top_ten_collection)
+def pygraph_reports(qty_documents,document_more_words,qty_distinct_words,top_ten_collectio):
+    try:
+        from matplotlib import pyplot as  plt
+
+        ## Tuplas necesarias para graficar
+        words = top_ten_collectio                         ## Se agrega con el objeto 
+        slicesWords = (100,90,87,80,70,50,30,25,22,10)    ## Se agrega con el objeto 
+        valuesWords = (0.06,0,0,0,0,0,0,0,0,0) ## Podria generarlo con una funcion
+       
+        ## Datos para caja de texto
+        distinct_words = qty_distinct_words
+        documents_qty = qty_documents
+        document_whit_more_words = document_more_words
+       
+        ## Caja de texto
+        text_box='\n\n Palabras Diferentes:\n > '+str(distinct_words)
+        text_box +='\n\n Documentos Totales:\n > '+str(documents_qty)
+        text_box +='\n\n Documento con mas palabras:\n > '+str(document_whit_more_words)+'\n\n' 
+
+        ## Ocultar Toolbar
+        plt.rcParams['toolbar']='None'
+
+        ## defino la figura
+        fig = plt.figure(0, figsize=(20,10))
+        ax = fig.add_subplot(111)
+      
+        ## defino el titulo general
+        plt.title('TOP 10 DE PALABRAS EN LA COLECCIÓN \n\n' , weight='bold' ,verticalalignment='center',fontsize=20 )
+       
+        ## defino el modo como se dibuja el grafico
+        ax.axis('equal')
+        sliceColorWords = ('#FFE800','#DFCD15','#C2B424','#9B912E','#817A32','#605C32','#504E34','#3C3B2E','#292925','#000000')
+        _, _, graph_text = ax.pie(slicesWords,
+                    colors=sliceColorWords,
+                    labels=words,
+                    autopct='%1.1f%%', ## Procentage y su formato del porcentage
+                    explode=valuesWords,
+                    startangle=90, ## Angulo
+                    )
+        ax.pie
+
+        ## Recoremos Los textos para setiarlos en blanco
+        for text in graph_text:
+            text.set_color('white')
+        
+
+        ## Agrego caja de texto, con resultado de las querys
+        y = 0.7
+        x = -2.4
+        plt.text(x, y, text_box, color='white', weight='bold', horizontalalignment='left', verticalalignment='center', bbox=dict(facecolor='#000000', alpha=0.9 ,boxstyle='round', edgecolor='#3C3B2E', pad=1) )
+
+        ## mustro pyplot que contiene todo los parametros ya cargados.
+        plt.show()
+
+    except Exception as e:
+        print(mark+"pygraph_reports: {}".format(e))
+
 
 ## Ejecucion principal del programa
 def main():
@@ -111,14 +170,18 @@ def main():
         processing_files(filesNames, path)
         ### CONSULTO LA CANDAD
         qty_documents = query_mongoDB_count_document()
-        qty_distinct_words  = query_mongoDB_distinct_words()
-        document_more_words = query_mongoDB_document_more_words()
-        top_ten_collection  = query_top_ten_collection()
+        ##qty_distinct_words  = query_mongoDB_distinct_words()
+        ##document_more_words = query_mongoDB_document_more_words()
+        ##top_ten_collection  = query_top_ten_collection()
+        #qty_documents = 5
+        qty_distinct_words  = 123123
+        document_more_words = "doc1.txt"
+        top_ten_collection  = ('pywords2','vuelvoloco','mas','menos','mona','chinverguencha','mono','zorro','GOy','Laa')
 
-        print("Cantidad documentos",qty_documents)
-        print("Documento con mas palabras",document_more_words)
-        print("Palabras distintas",qty_distinct_words)
-        print("Top ten de palabras",top_ten_collection)
+        #####################################################################print("Cantidad documentos",qty_documents)
+        #####################################################################print("Documento con mas palabras",document_more_words)
+        #####################################################################print("Palabras distintas",qty_distinct_words)
+        #####################################################################print("Top ten de palabras",top_ten_collection)
 
         ## Generar reporte grafico
         pygraph_reports(qty_documents,document_more_words,qty_distinct_words,top_ten_collection)
